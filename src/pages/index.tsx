ceedbd612bcc0cb1838101f8b1c19452ee2576ee
Josh Hoover <caboodlennm@gmail.com>
2023-03-09 16:11:50 -0600

    Selectability of stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="relative min-h-screen overflow-hidden bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="flex grow flex-col items-center text-white gap-40 mt-4">
+        <div className="flex grow flex-col items-center text-white gap-40 mt-4 select-none">
           <h1 className="text-4xl">Science Based 100% Dragon MMO (Monotonous Metacarpal Offender)</h1>
           <Game />
         </div>
